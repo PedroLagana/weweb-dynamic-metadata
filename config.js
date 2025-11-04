b9a0338e10a75f72ee0b75eaa1193c762e5b6412
PedroLagana <38016695+PedroLagana@users.noreply.github.com>
2025-11-04 15:44:45 -0300

    Update domainSource and add new pattern metadata

--- config.js
+++ config.js
@@ -1,13 +1,9 @@
 export const config = {
-  domainSource: "https://f69a71f6-9fd8-443b-a040-78beb5d404d4.weweb-preview.io", // Your WeWeb app preview link
+  domainSource: "https://2c48577f-6ece-45c4-9197-0fb036885fc0.weweb-preview.io", // Your WeWeb app preview link
   patterns: [
       {
-          pattern: "/event/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:8wD10mRd/event/{id}/meta"
-      },
-      {
-          pattern: "/team/profile/[^/]+",
-          metaDataEndpoint: "https://xeo6-2sgh-ehgj.n7.xano.io/api:LjwxezTv/team/profile/{profile_id}/meta"
+          pattern: "/produto/[^/]+",
+          metaDataEndpoint: "https://aszvggegvmuzybjdeftf.supabase.co/rest/v1/rpc/get_product_metadata?id={id}&apikey=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImFzenZnZ2Vndm11enliamRlZnRmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDgyNzY1MjksImV4cCI6MjA2Mzg1MjUyOX0.i1DNye0LpG6P33gFdtDiOKEjR6ffHnF-wGrpPTFyIsk"
       }
       // Add more patterns and their metadata endpoints as needed
   ]
